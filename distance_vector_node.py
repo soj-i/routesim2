1587883be8d35c733a1ab8abbf07f94b335b0e33
soj-i <sojioduneye2025@u.northwestern.edu>
2025-02-28 23:52:43 -0600

    final dv

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -1,20 +1,12 @@
 import copy
 import json
+
 from simulator.node import Node
 
-
-'''
-class Node:
-    def __init__(self, id):
-        self.id = id
-        self.neighbors = []
-        self.logging = logging.getLogger('Node %d' % self.id)
-'''
 class Distance_Vector_Node(Node):
     def __init__(self, id):
         super().__init__(id)
         self.logging.debug("new node %d" % self.id)
-
         '''
         - initialization refers only to self and neighbors
 
@@ -26,12 +18,13 @@ class Distance_Vector_Node(Node):
             2 updates - new dv, or direct links have been updated
                     - call bellman to recalculate distances
         '''
-        self.self_dictionary = {self.id: [0,[self.id]]} # self_dictionary k: node_id v: nodes for path
+        self.self_dictionary = {} # self_dictionary k: node_id v: nodes for path
 
-        self.neighbor_dv_dictionary = {} # node_id :[cost, [path]]|
-        self.links_to_neighbor = {} # (src,dst) : latency
-        self.sequence_number = 0
-        self.neighbor_sequence_numbers = {} # node_id : most recent seq
+        self.neighbor_dvs = {}  # -> k: neighbor, v: DV_dict (k: dst: [cost, [path])
+
+        self.neighbor_seq_nums = {} # node_id : most recent seq
+        self.seq_num = 0
+        self.link_costs = {} 
 
     # Return a string
     def __str__(self):
@@ -41,17 +34,14 @@ class Distance_Vector_Node(Node):
         sensible information. This function should return a string.
         '''
         "Rewrite this function to define your node dump printout"
-        return "A DV Node: " + str(self.id) + "\n"
-        '''
-        is called to when the simulation wants to print a representation of the node's state for debugging.
-        This is not essential, but it may be helpful to implement this function so that DUMP_NODE events print
-        sensible information. This function should return a string.
-        '''
-        "Rewrite this function to define your node dump printout"
-        return "A DV Node: " + str(self.id) + "\n"
+        message = {}
+        for neighbor, dv in self.self_dictionary.items():
+            message[neighbor] = {"cost": dv["cost"], "path": dv["path"]}
 
-    # Fill in this function
-    
+        message[-1] = self.seq_num
+        self.seq_num += 1
+
+        return copy.deepcopy(json.dumps(message))
 
     def link_has_been_updated(self, neighbor, latency):
         '''
@@ -60,77 +50,102 @@ class Distance_Vector_Node(Node):
         In response, you may want to update your tables and send further messages to your neighbors. 
         This function does not have to return anything.
         '''
-        link_candidate = frozenset((self, neighbor))
+        # link_candidate = (self, neighbor)
+
         # latency = -1 if delete a link
         if latency == -1:
             # remove link from links dictionary 
-            self.links_to_neighbor.pop(link_candidate) # remove self-neighbor link
-            self.neighbor_dv_dictionary.pop(neighbor) # remove from MY copy of neighbor's dv
+            self.link_costs.pop(neighbor)
+            self.neighbor_dvs.pop(neighbor)
 
         else:
-            if link_candidate not in self.links_to_neighbor or self.links_to_neighbor[link_candidate] != latency: # link not in current set
-                self.links_to_neighbor[link_candidate] = latency # or if link is present AND new latency, recalc own dv
+            self.link_costs[neighbor] = latency
 
-                self.self_dictionary[neighbor] = [latency, [neighbor]] # update neighbor's cost in self dv
-            
-                # recalculate self dv(?)
+        self.recompute_dvs()
 
-            pass
-    
-    def recalculate_distance_vector(self):
-        old_dv = copy.deepcopy(self.self_dictionary)
+    def recompute_dvs(self):
+        old_dvs = copy.deepcopy(self.self_dictionary)
+        self.self_dictionary = {}
 
-        for neighbor_node, dv in self.neighbor_dv_dictionary.items():
-            for dst, (cost, full_path) in dv.items():
-                new_cost =  cost + self.links_to_neighbor[frozenset((self,neighbor_node))]
-                if dst not in self.self_dictionary or self.self_dictionary[dst][0] > new_cost:
-                    self.self_dictionary[dst] = [new_cost, [self] + [full_path]]
-        if old_dv != self.self_dictionary: # if any updates to optimal dv
+        for neighbor, link_cost in self.link_costs.items():
+            self.self_dictionary[neighbor] = {"cost": link_cost, "path": [self.id, neighbor]}
+
+        for source, dvs in self.neighbor_dvs.items():
+            for destination, dv in dvs.items():
+                self.recompute_single_dv(src=source, dst=destination, dv=dv)
+
+        if self.self_dictionary != old_dvs:
             self.broadcast_to_neighbors()
-    
+
+    def recompute_single_dv(self, src: int, dst: int, dv: dict):
+        if src not in self.link_costs:
+            return
+        new_cost = dv["cost"] + self.link_costs[src]
+        if dst not in self.self_dictionary or (dst in self.self_dictionary and new_cost < self.self_dictionary[dst]["cost"]):
+            new_path = [self.id] + copy.deepcopy(dv["path"])
+            self.self_dictionary[dst] = {"cost": new_cost, "path": new_path}
+
     def broadcast_to_neighbors(self):
-        message = {"seq_num": self.sequence_number, "dv": self.self_dictionary}
-        self.sequence_number += 1
-        json_message = json.dumps(message)
-        self.send_to_neighbors(json_message)
+        self.send_to_neighbors(str(self))
 
 
-
-    # Fill in this function
     def process_incoming_routing_message(self, m):
-        '''
-        is called when a routing message "m" arrives at a node. This message would have been sent by a neighbor
-        (more about how to do that later). The message is a string. In response, you may send further routing messages 
-        using self.send_to_neighbors or self.send_to_neighbor. You may also update your tables. 
-        This function does not have to return anything.
-        '''
-        dict_message = json.loads(m) # json obj -> dict
-        seq_num = dict_message["seq_num"] 
-        neigh_dv = dict_message["dv"]
+        neighbor_self_dvs = json.loads(m)
+        seq_num = neighbor_self_dvs['-1']
 
-        neighbor = list(neigh_dv.keys())[0] # first key should be neighbor node (?)
-        
-        if neighbor not in self.neighbor_sequence_numbers or seq_num > self.neighbor_sequence_numbers[neighbor]:
-            self.neighbor_sequence_numbers[neighbor] = seq_num #update neighbor's sq num
-            self.neighbor_dv_dictionary[neighbor] = neigh_dv #update neighbor's dv
+        del neighbor_self_dvs['-1']
 
-            self.recalculate_distance_vector() # recalc since update detected
+        changed = False
 
-    # Return a neighbor, -1 if no path to destination
+        neighbor = int(neighbor_self_dvs[next(iter(neighbor_self_dvs))]['path'][0])
+        to_delete = []
+        if neighbor not in self.neighbor_dvs:
+            self.neighbor_dvs[neighbor] = {}
+
+        if neighbor in self.neighbor_seq_nums and seq_num < self.neighbor_seq_nums[neighbor]:
+            return
+
+        for dst, value in copy.deepcopy(self.neighbor_dvs[neighbor]).items():
+            to_delete.append(dst)
+
+        for dst_str, value in neighbor_self_dvs.items():
+            dst = int(dst_str)
+            if dst in to_delete:
+                to_delete.remove(dst)
+
+            link = {"cost": value["cost"], "path": value["path"]}
+
+            changed = self.process_neighbor_dv(src=neighbor, dst=dst, dv=link, seq_num=seq_num) or changed
+
+        for dst in to_delete:
+            del self.neighbor_dvs[neighbor][dst]
+            changed = True
+
+        self.neighbor_seq_nums[neighbor] = seq_num
+
+        if changed:
+            self.recompute_dvs()
+
+    def process_neighbor_dv(self, src, dst, dv, seq_num):
+        if dst in self.neighbor_dvs[src]:
+            if self.id in dv["path"]:
+                del self.neighbor_dvs[src][dst]
+                return True
+
+            self.neighbor_dvs[src][dst] = dv
+            return True
+        else:
+            if self.id in dv["path"]:
+                return False
+            else:
+                self.neighbor_dvs[src][dst] = dv
+                return True
 
     def get_next_hop(self, destination):
-        '''
-        is called when the simulator wants to know what your node currently thinks is the next hop on the 
-        path to the destination node. You should consult your routing table or whatever other mechanism you
-        have devised and then return the correct next node for reaching the destination.
-        This function should return an integer.
-        '''
-        
-        if destination in self.self_dictionary: #destination = { node : [cost , [path]}
-            print(self.self_dictionary)
-            path = self.self_dictionary[destination][1]
-            
-            if len(path) > 1: #
-                return path[1]
+        # print("HELLOO")
+        # print(self.self_dictionary)
+        if destination in self.self_dictionary:
+            if self.self_dictionary[destination]["cost"] < float('inf'):
+                return copy.deepcopy(self.self_dictionary[destination]["path"])[1]
 
-        return 0 # error(?)
+        return -1
